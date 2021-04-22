0bf5f671110d0b921fe56024df6d4f67dcbd7a27
Kaung Shein <nicholasling1412@gmail.com>
2021-04-22 17:33:23 -0400

    added update and delete routes for Contacts. Contacts finalized

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   # Contact-related routes
   post '/contact/create', to:'contacts#createContact'
+  put '/contact/:id', to:'contacts#updateContact'
+  delete '/contact/:id', to: 'contacts#deleteContact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
