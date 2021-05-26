645790659d626bcc6111640bda263f97672da810
Kaung Shein <nicholasling1412@gmail.com>
2021-05-25 21:12:13 -0400

    Added Excitement to params

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -54,6 +54,6 @@ class JobsController < ApplicationController
     end
 
     def job_params
-      params.require(:job_info).permit(:status, :job_title, :job_industry, :company_name, :date_applied, :time_phonescreen, :date_offered, :date_accepted, :date_companyrejection, :date_userrejection, :job_description, :resume, :coverletter, :location_city, :location_state)
+      params.require(:job_info).permit(:status, :job_title, :job_industry, :company_name, :date_applied, :time_phonescreen, :date_offered, :date_accepted, :date_companyrejection, :date_userrejection, :job_description, :resume, :coverletter, :location_city, :location_state, :excitement)
     end
 end
